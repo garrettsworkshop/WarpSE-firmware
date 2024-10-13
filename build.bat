800b53e1696bfb454e2f74aa99091fcede866161
Zane Kaminski <zanekaminski@gmail.com>
2024-10-13 02:49:10 -0400

    working on build script

--- build.bat
+++ build.bat
@@ -1,13 +1,21 @@
-@echo off
+x@echo off
 set origfolder=%cd%
 
+
 FOR /D /r %%G in ("XC95144XL\*") DO (
-	cd %%G
-	if not exist "build" mkdir build
-	cd build
-	C:\Xilinx\14.7\ISE_DS\ise\bin\nt\xst -ifn ../WarpSE.xst
-	rem C:\Xilinx\14.7\ISE_DS\ise\bin\nt\ngdbuild -uc $../../../WarpSE.ucf bin/WarpSE.ngc -p XC95144XL
-	rem C:\Xilinx\14.7\ISE_DS\ise\bin\nt\cpldfit -p XC95144XL -ofmt verilog -optimize speed WarpSE
-	rem C:\Xilinx\14.7\ISE_DS\ise\bin\nt\hprep6 -s IEEE1149 -n WarpSE -i WarpSE
+	cd /d %%G
+	if exist build del /f /s /q build
+	if exist build rmdir /s /q build
+	mkdir build
+	cd /d build
+	C:\Xilinx\14.7\ISE_DS\ise\bin\nt\xst -ifn ../../../WarpSE.xst
+	C:\Xilinx\14.7\ISE_DS\ise\bin\nt\ngdbuild -uc ../../../WarpSE.ucf WarpSE.ngc -p XC95144XL-10-TQ100
+	C:\Xilinx\14.7\ISE_DS\ise\bin\nt\cpldfit -p XC95144XL-10-TQ100 -ofmt verilog -optimize speed WarpSE
+	C:\Xilinx\14.7\ISE_DS\ise\bin\nt\hprep6 -s IEEE1149 -n WarpSE -i WarpSE
+	C:\Xilinx\14.7\ISE_DS\ise\bin\nt\impact -batch ../../../WarpSE-SVF.impact
+	copy WarpSE.svf ..\
+	copy WarpSE.jed ..\
+	cd /d %origfolder%
 )
+
 cd /d %origfolder%
--- clean.bat
+++ clean.bat
@@ -1,8 +1,11 @@
 @echo off
 set origfolder=%cd%
 
+
 FOR /D /r %%G in ("XC95144XL\*") DO (
-	cd %%G
+	cd /d %%G
 	del /f /s /q build
+	rmdir /s /q build
 )
+
 cd /d %origfolder%
